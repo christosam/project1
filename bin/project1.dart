bd3b255a09f60a72442e4d929326d4eb5be6afbd
Christo Sam Mathew <christomathew30@gmail.com>
2022-03-10 12:57:49 +0530

    Initial commit

---
+++ bin/project1.dart
@@ -0,0 +1,20 @@
+import "dart:io";
+
+void main(List<String> arguments) {
+  print('enter the number');
+  var n1 = int.parse(stdin.readLineSync().toString());
+
+  for (n1= )
+  {
+    case 1:
+
+        print("one");
+        break;
+    case 2 :
+        print("two");
+        break;
+    case 3: print("three");
+    break;
+
+  }
+}
